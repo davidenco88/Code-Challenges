4584a119837916d74cea9fc737d6863939b74aba
davidenco88 <122131492+davidenco88@users.noreply.github.com>
2024-01-04 21:38:14 +0100

    feat: :tada: Start function declaration challenge_25

---
+++ advent_js_2023/challenge_25/travelDistance.js
@@ -0,0 +1,23 @@
+function travelDistance(map) {
+  return 0;
+}
+
+// --------TESTS--------------
+
+const map = `.....1....
+..S.......
+..........
+....3.....
+......2...`;
+
+const result = travelDistance(map);
+console.log(result); // -> 12 km
+/*
+De la S al niño 1: 4 movimientos
+Del niño 1 al 2: 5 movimientos
+Del niño 2 al 3: 3 movimientos
+Total: 12 movimientos
+*/
+
+const result2 = travelDistance(`..S.1...`);
+console.log(result2); // -> 2
